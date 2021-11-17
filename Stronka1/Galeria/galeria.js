44420e077b685cd422945e1a11885b3efc12c29f
Dawid <dejwid.sz.yt@gmail.com>
2021-11-17 09:40:42 +0100

    Wiecej stron bedize se

---
+++ Stronka1/Galeria/galeria.js
@@ -0,0 +1,17 @@
+var tabButtons=document.querySelectorAll(".tabContainer1 .buttonContainer .mini-galeria button");
+var tabPanels=document.querySelectorAll(".tabContainer2 .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="rgb(113, 113, 113)";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'transparent');
