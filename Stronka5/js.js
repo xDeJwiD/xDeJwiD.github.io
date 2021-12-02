a802a91546f50e947a7c100969488a5de5d4caab
Dawid <dejwid.sz.yt@gmail.com>
2021-12-02 10:44:23 +0100

    znowu gowno

--- Stronka5/js.js
+++ Stronka5/js.js
@@ -2,17 +2,15 @@ function test() {
     document.getElementById('h').innerHTML = ''
     
     var cos = document.getElementById('pesel').value
+    var dlu = document.getElementById('pesel').lenght
 
     
 
-    if (cos.lenght != 11) {
-        document.getElementById('h').innerHTML =''
-        document.getElementById('h').innerHTML ='nie'
-
-    }
-    if (cos.lenght == 11) {
-        document.getElementById('h').innerHTML =''
+    if (cos.value == 11) {
         document.getElementById('h').innerHTML ='Tak :D'
+
+    } else {
+        document.getElementById('h').innerHTML ='Nie :P'
     }
     // if (cos.lenght == 11) {
     //     document.getElementById('h').innerHTML =''
@@ -20,14 +18,12 @@ function test() {
 
     // } 
 
-    if (cos.include('-') == true) {
+    
 
-    }
+    // if (cos [2] > 3) {
+    //     document.getElementById('h').innerHTML+='nie nie nie'
+    //     return 2
+    // }
 
-    if (cos [2] > 3) {
-        document.getElementById('h').innerHTML+='nie nie nie'
-        return 2
-    }
-
-    cos[9] % 2 == 0 ? document.getElementById('plec').innerHTML = 'kobieta' : document.getElementById('plec').innerHTML = "mezczyzna"
+    // cos[9] % 2 == 0 ? document.getElementById('plec').innerHTML = 'kobieta' : document.getElementById('plec').innerHTML = "mezczyzna"
 }
