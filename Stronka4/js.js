19a469825a554bdd1220d06f868ff742bb586383
Dawid <dejwid.sz.yt@gmail.com>
2021-12-08 10:55:24 +0100

    rysowanko

--- Stronka4/js.js
+++ Stronka4/js.js
@@ -1,31 +1,31 @@
-var ruszanie = document.querySelector('#kolo');
+// var ruszanie = document.querySelector('#kolo');
 
-document.onmousemove = (event) => {
-    var x = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
+// document.onmousemove = (event) => {
+//     var x = event.clientX * 100 / window.innerWidth + "%";
+//     var y = event.clientY * 100 / window.innerHeight + "%";
     
-    kolo.style.transition = "0s";
-    kolo.style.left = x;
-    kolo.style.top = y;
-}
+//     kolo.style.transition = "0s";
+//     kolo.style.left = x;
+//     kolo.style.top = y;
+// }
 
-document.onmouseout = (event) => {
-    kolo.style.transition = "0.7s";
-    kolo.style.left = "50%";
-    kolo.style.top = "50%";
-}
+// document.onmouseout = (event) => {
+//     kolo.style.transition = "1s";
+// }
 
-document.getElementById("pole")
-function cos() {
-  var div = document.createElement('div');
-     div.style.backgroundColor = "pink";
-     div.style.position = "absolute";
-     div.style.transform = "translate(-50%, -50%)"
-     div.style.left = kolo.style.left;
-     div.style.top = kolo.style.top;
-     div.style.height = "10px";
-     div.style.width = "10px";
-     div.style.borderRadius = "50rem"
+// document.getElementById("pole")
+// function cos() {
+//   var div = document.createElement('div');
+//      div.style.backgroundColor = "pink";
+//      div.style.position = "absolute";
+//      div.style.transform = "translate(-50%, -50%)"
+//      div.style.left = kolo.style.left;
+//      div.style.top = kolo.style.top;
+//      div.style.height = "10px";
+//      div.style.width = "10px";
+//      div.style.borderRadius = "50rem"
 
-     document.getElementsByTagName('body')[0].appendChild(div);
-};
+//      document.getElementsByTagName('body')[0].appendChild(div);
+// };
+
+
