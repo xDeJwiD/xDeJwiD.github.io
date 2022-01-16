4d5573e7b419f3e52dcba8f8626f1c50fc81a3bd
xDeJwiD <dejwid.sz.yt@gmail.com>
2022-01-16 23:39:34 +0100

    chuj

--- zerdzinski/scropt.js
+++ zerdzinski/scropt.js
@@ -1,12 +1,12 @@
-var main = document.querySelector('.main')
-var games = document.querySelector('.games')
+// var main = document.querySelector('.main')
+// var games = document.querySelector('.games')
 
-function poka() {
-    main.classList.add('hidden');
-    games.classList.remove('hidden');
-}
+// function poka() {
+//     main.classList.add('hidden');
+//     games.classList.remove('hidden');
+// }
 
-function usun() {
-    games.classList.add('hidden');
-    main.classList.remove('hidden');
-}
+// function usun() {
+//     games.classList.add('hidden');
+//     main.classList.remove('hidden');
+// }
