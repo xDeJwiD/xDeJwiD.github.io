21ad4d238ef3a315b62a9fb8dff549976bc96b55
Dawid <93331671+xDeJwiD@users.noreply.github.com>
2025-06-03 22:23:17 +0200

    Bug fixes

--- s.js
+++ s.js
@@ -7,12 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const backgroundVideos = [
     "./img/vdo/Mod.webm", // Sekcja 1
-    "./img/vdo/Gopro.webm", // Sekcja 2
-    "./img/vdo/Arch.webm", // Sekcja 3
-    "./img/vdo/Nuke.webm", // Sekcja 4
+    "./img/vdo/Gopro.webm", // Sekcja 2    
+    "./img/vdo/Nuke.webm", // Sekcja 3
+    "./img/vdo/Arch.webm", // Sekcja 4
     "./img/vdo/Nothing.webm", // Sekcja 5
     "./img/vdo/F1.webm", // Sekcja 6
-    "./img/vdo/Wheel.webm", // Sekcja 7
+    "./img/vdo/Wheel1.webm", // Sekcja 7
   ];
 
   const sectionsmoreshown = document.querySelectorAll(".section-more");
